a4205479c046decd517a1ba14dd4309adf2afb76
rapidmon <ehhwll@hanmail.net>
2022-04-17 12:19:16 +0900

    로또 번호 추첨

---
+++ 로또번호 추첨기/index.js
@@ -0,0 +1,33 @@
+//lotto 번호 생성
+const lotto = [];
+for(let i =0; i<6; i++){
+    const num = parseInt(Math.random()*45+1);
+
+    //중복 번호 제거
+    if(lotto.indexOf(num) == -1){
+        lotto.push(num);
+    }
+    else{
+        i -=1;
+    }
+}
+
+//lever 내리기
+const $lever = document.getElementById('circle')
+
+$lever.addEventListener('click', function(e){
+    e.preventDefault;
+
+    $lever.style.top = "200px"
+    document.querySelector(".bar").style.transform = "rotateX(180deg)"
+    document.querySelector(".bar").style.top = "130px"
+    for(let i =0; i<6; i++){
+        document.querySelector(".lotto").childNodes[i*2+1].classList.remove("bounce");
+        document.querySelector(".lotto").childNodes[i*2+1].classList.add("bounce");
+        document.querySelector(".lotto").childNodes[i*2+1].style.transform = "translateY(250px)"        
+        document.querySelector(".lotto").childNodes[i*2+1].style.backgroundColor = "#fafad2"
+    }
+    for(let i =0; i<6; i++){
+        document.querySelector(".lotto").childNodes[i*2+1].textContent = lotto[i]
+    }
+}, false)
